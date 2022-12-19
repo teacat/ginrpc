bcb16fbcf4b7a65100a8360651b6a476c89255a1
YamiOdymel <yamiodymel@gmail.com>
2022-12-19 19:54:34 +0800

    NewForm

--- ginrpc.go
+++ ginrpc.go
@@ -4,12 +4,29 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 )
 
 func New[T any, M any](handler func(*gin.Context, T) (M, error)) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var req T
 		if err := c.BindJSON(&req); err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		resp, err := handler(c, req)
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		c.JSON(http.StatusOK, resp)
+	}
+}
+
+func NewForm[T any, M any](handler func(*gin.Context, T) (M, error)) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var req T
+		if err := c.BindWith(&req, binding.FormMultipart); err != nil {
 			c.AbortWithError(http.StatusInternalServerError, err)
 			return
 		}
